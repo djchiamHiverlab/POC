7d22428f4bcc1f93a6a4d2b07e84e29828f13673
DJChiam_Hivelab <hiverlab.djchiam@gmail.com>
2024-03-31 23:40:15 +0800

    refactor: Changed Comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import './App.css';
 
 const App = () => {
     const [selectedFile, setSelectedFile] = useState(null);
-    const [showVideo, setShowVideo] = useState(false); // New state to control video visibility
-    // Assuming videoUrl is your pre-generated video URL
+    const [showVideo, setShowVideo] = useState(false); 
+    
     const videoUrl = 'https://files.heygen.ai/aws_pacific/avatar_tmp/cb0e683949aa4e3fa2df4c97cc7ce91a/d89350cdd7434e619d98e155269f4f06.mp4?Expires=1712250871&Signature=MiKGlaaVmUGnqRuGiqNZc3Hfbx~BXTfOrHYACc-CIBEkRPGAeT5RRfbX4tiHGqCgnXrFOTwwZku73svIiL~M6oIc5CZdmdQjiImoSKpSnj6WF3fmY25tXygthCIp8fq3HpzYNW0B4m49MFwEPevPhS3F6YMytLaAlmQGdgzo-dCumWWHBvx3uVafkOcaPIr8~6ycBQF11BT3Xc6Ns-T~qKiTL5CiRuDWk5Q-nBeRRJRqCpKsE2Z0hkEzjDJ1iPncQuw~UB5csVL89LNepMi0TzEtfYhDuTCHXQvrndznVc~07VUQPIysR7rv-qPlV3ZWOCIchQnEqgT5~u8hHuoCBA__&Key-Pair-Id=K49TZTO9GZI6K';
 
     const onFileChange = event => {
         const file = event.target.files[0];
-        if (file && file.size > 50 * 1024 * 1024) { // Check for 50MB
+        if (file && file.size > 50 * 1024 * 1024) { 
             alert("File size cannot exceed more than 50MB");
         } else {
             setSelectedFile(file);
-            setShowVideo(false); // Reset video visibility when a new file is selected
+            setShowVideo(false); 
         }
     };
 
